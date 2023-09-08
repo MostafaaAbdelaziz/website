927868626470aa867a791a2c9138fd94496d03db
Mostafaa Abdelaziz <MostafaaAbdelaziz@dal.ca>
2023-09-07 23:15:25 -0300

    final commit
    finished website and ready to be aired

---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleMenu(){
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
+
+function openVideoPopup(videoSource) {
+    const videoPopup = document.getElementById("videoPopup");
+    const videoPlayer = document.getElementById("videoPlayer");
+
+    videoPlayer.src = videoSource;
+    videoPopup.style.display = "block";
+}
+
+function closeVideoPopup() {
+    const videoPopup = document.getElementById("videoPopup");
+    videoPopup.style.display = "none";
+}
+
+// Assign click event listeners to project buttons
+document.getElementById("project1Btn").addEventListener("click", function () {
+    openVideoPopup("./assets/Demo-of-WOWJava.mp4");
+});
+
+document.getElementById("project2Btn").addEventListener("click", function () {
+    openVideoPopup("./assets/demoMultiClient.mp4");
+});
+
+document.getElementById("project3Btn").addEventListener("click", function () {
+    openVideoPopup("./assets/demoMultiServer.mp4");
+});
